b28eefb6c470ddae73f184f0f261884bf2ca23ac
maximejournet54 <maxime.journet@free.fr>
2020-01-01 16:58:50 +0100

    tests

---
+++ Genere_Grille.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i, j, nlig, ncol;
+        /* remplacer 5 par nlig et ncol*/
+    for (i=5; i<=30; i++ )
+    {
+           printf("_"); 
+    }
+        for (j=5; j<=5; j++)
+        {
+            printf("|"); printf("\t");
+        }
+    return 0;
+}
---
+++ Jeu_Nim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+    {
+        int nlig, ncol;
+        printf("nombres de lignes =:");
+        scanf("%d", &nlig);
+        printf("nombre de colonnes =:");
+        scanf("%d", &ncol);
+    }
