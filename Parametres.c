d5d7c57798a08dcff59e40e42f98044b2e49cd57
maximejournet54 <maxime.journet@free.fr>
2020-01-02 19:26:11 +0100

    ajout Max+Parametres

--- Parametres.c
+++ Parametres.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <time.h>
+#include "max.c"
 
 int main ()
 {
-
+    int nlig, ncol, niveau, next, nban;
+    srand((unsigned int)time(NULL));
+    printf("saisir le nombre de lignes");
+    scanf("%d", &nlig);
+    printf("saisir le nombre de colonnes");
+    scanf("%d", &ncol);
+    printf("saisir le niveau de difficulté");
+    scanf("%d", &niveau);
+    printf("saisir qui commence la partie: 1=ordinateur, 2=joueur");
+    scanf("%d", &next);
+    if(rand()<Max(nlig,ncol))   nban=srand();
 }
+
---
+++ max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+    int nlig, ncol, max, i, n;
+    scanf("%d %d", &nlig, &ncol);
+    for (i=2; i<=n; ++i)
+    {
+        if (nlig>ncol)
+        max=nlig;
+        else
+        {
+            max=ncol;
+        }
+        
+    }
+}
