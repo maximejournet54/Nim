d08eda2afacc8037134fe334bfd5936271374d96
maximejournet54 <maxime.journet@free.fr>
2020-01-15 15:47:47 +0100

    ajout commentaires

--- jeu.h
+++ jeu.h
@@ -14,18 +14,18 @@ struct T_Tab_Case
 };
 
 /* prototype des différentes fonctions*/
-struct T_Tab_Case ban, vois;
+struct T_Tab_Case ban, vois; 
 struct T_Case pion;
 struct T_Case case_;
 int nim[VMAX][VMAX]; /* table contenant les coordonnées des nimbers*/
-void Voisines(struct T_Case, int*, struct T_Tab_Case *, int, struct T_Tab_Case, int, int);
-int Parametres(int*, int*, int*, int*, int*, int); 
-int Lire_Entier(int, int);
-int Max(int*, int*);
+void Voisines(struct T_Case, int*, struct T_Tab_Case *, int, struct T_Tab_Case, int, int); /*détermine les cases voisines de la case où se trouve le pion */
+int Parametres(int*, int*, int*, int*, int*, int);  /* saisie des paramètres de jeu */
+int Lire_Entier(int, int); /* lit l'entier saisit par l'utilisateur*/
+int Max(int*, int*); /*calcule le maximum de 2 entiers */
 int Affiche_Grille(struct T_Case, struct T_Tab_Case, int, int, int); /* affiche la grille de jeu à chaque tour*/
 struct T_Case Coup_Ordi_Hasard(struct T_Case,int,struct T_Tab_Case,int,int); /* génère pseudo-aléatoirement le coup de l'ordi si le coup n'est pas gagnant */ 
 struct T_Case Coup_Ordi_Gagnant(struct T_Case,int,struct T_Tab_Case,int,int,int nim[][VMAX]); /* génère le coup gagnant de l'ordi */
-struct T_Case  Coup_Joueur(struct T_Case, int, struct T_Tab_Case, int, int); /* texte affiché à chaque tour du joueur */ 
+struct T_Case Coup_Joueur(struct T_Case, int, struct T_Tab_Case, int, int); /* gère le coup choisi par le joueur */ 
 void Calcul_Nimbers(int nim[][VMAX], int, int, int, struct T_Tab_Case);   /* remplit la grille au début de partie par des 0 et des 1 */
 struct T_Tab_Case Hasard_Ban(int, int *, int *); /* génère pseudo-aléatoirement les cases bannies, compris entre 1 et nban*/ 
 
--- main.c
+++ main.c
@@ -9,32 +9,31 @@
 int main ()
 {
     /* début du jeu */
-    int nlig, ncol, nban, niveau, i, j, next, null=0, verif=1, grille, p;
+    int nlig, ncol, nban, niveau, i, j, next, verif=1, grille, p; /* p=probabilité que l'ordinateur joue un coup gagnant  */
     pion.ilig=1;
 	pion.icol=1;
-    srand((unsigned int)time(NULL));
-    Parametres(&nlig, &ncol, &nban, &niveau, &next, verif); 
-    while (verif==1) /* s'éxécute si les paramètres sont corrects */
+    srand((unsigned int)time(NULL)); /* initialisation de la graine */
+    Parametres(&nlig, &ncol, &nban, &niveau, &next, verif); /* saisie des paramètres de jeu */
+    while (verif==1) /* s'éxécute si les paramètres saisis sont corrects */
     {
-        ban=Hasard_Ban(nban,&nlig,&ncol);
-        printf("%d %d %d %d %d",nlig,ncol,niveau,next,nban);
+        ban=Hasard_Ban(nban,&nlig,&ncol); /* génère pseudo-aléatoirement les coordonnées des cases bannies*/
 	    for(i=0;i<nban;i++)
         {
 		printf("\n %d : ligne", ban.tab[i].ilig);
 		printf("\n %d : colonne", ban.tab[i].icol);		
 	    }
-	    printf("\n position pion : (%d,%d)\n",pion.ilig,pion.icol);
-        grille=Affiche_Grille(pion,ban,nlig,ncol,nban);
-        Calcul_Nimbers(nim,nlig,ncol,nban,ban);
+	    printf("\n position pion : (%d,%d)\n",pion.ilig,pion.icol); 
+        grille=Affiche_Grille(pion,ban,nlig,ncol,nban); /*affichage de la grille de jeu */
+        Calcul_Nimbers(nim,nlig,ncol,nban,ban); 
     while(1)
 	{
-		if(next%2==0)
+		if(next%2==0) /* cas où le joueur commence la partie */
 		{
 			pion=Coup_Joueur(pion,nban,ban,nlig,ncol);
 		}
-		else
+		else		 /* cas où l'ordinateur commence la partie */
 		{
-			if(niveau==1)
+			if(niveau==1) 	/* coup de l'ordinateur selon le niveau de difficulté*/
 			{
 				pion=Coup_Ordi_Hasard(pion,nban,ban,nlig,ncol);
 			}
@@ -58,7 +57,7 @@ int main ()
 				pion=Coup_Ordi_Gagnant(pion,nban,ban,nlig,ncol,nim);
 		}
 		grille=Affiche_Grille(pion,ban,nlig,ncol,nban);
-		if(pion.ilig==nlig && pion.icol==ncol)
+		if(pion.ilig==nlig && pion.icol==ncol) /* fin de la partie */
 		{
 			if(next%2==0)
             {
